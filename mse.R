ca8384e356bdc686dd2305d6ff88c71c909e03c1
mayer79 <mayermichael79@gmail.com>
2020-03-17 21:56:16 +0100

    started with mse bench

--- mse.R
+++ mse.R
@@ -1,11 +1,15 @@
+library(ranger)
+library(randomForestSRC)
+library(Rborist)
+library(h2o)
 library(xgboost)
 library(lightgbm)
-library(ranger)
+
+library(MetricsWeighted)
+library(splitTools)
 
 set.seed(1)
-n <- 1000
-set.seed(1)
-n <- 1000
+n <- 10000
 x1 <- seq_len(n)
 x2 <- rnorm(n)
 x3 <- rexp(n)
@@ -13,64 +17,116 @@ x4 <- runif(n)
 X <- cbind(x1, x2, x3, x4)
 y <- rnorm(n, x1 / 1000 + x2 / 10 + x3 / 5)
 
+idx <- partition(y, p = c(train = 0.7, valid = 0.3))
+X_train <- X[idx$train, ]
+y_train <- y[idx$train]
+X_valid <- X[idx$valid, ]
+y_valid <- y[idx$valid]
+
+mtry <- floor(sqrt(ncol(X)))
+mtry_p <- mtry / ncol(X)
+
 # XGB Random Forest
-param_xgb <- list(max_depth = 10,
-                  learning_rate = 1,
-                  objective = "reg:linear",
-                  subsample = 0.63,
-                  lambda = 0,
-                  alpha = 0,
-                  colsample_bynode = 1/3)
 
-dtrain_xgb <- xgb.DMatrix(X, label = y)
-
-fit_xgb <- xgb.train(param_xgb,
-                     dtrain_xgb,
-                     nrounds = 1,
-                     num_parallel_tree = 500)
+system.time({
+  param_xgb <- list(max_depth = 10,
+                    learning_rate = 1,
+                    objective = "reg:linear",
+                    subsample = 0.63,
+                    lambda = 0,
+                    alpha = 0,
+                    min_child_weight = 5,
+                    # tree_method = "hist",
+                    # base_score
+                    # max_delta_step
+                    colsample_bynode = mtry_p)
+  
+  dtrain_xgb <- xgb.DMatrix(X_train, label = y_train)
+  
+  fit_xgb <- xgb.train(param_xgb,
+                       dtrain_xgb,
+                       nrounds = 1,
+                       num_parallel_tree = 500)
+  pred_xgb <- predict(fit_xgb, X_valid)
+})
 
 
 # LGB Random Forest
-param_lgb <- list(boosting = "rf",
-                  max_depth = 10,
-                  num_leaves = 1000,
-                  learning_rate = 1,
-                  objective = "regression",
-                  bagging_fraction = 0.63,
-                  bagging_freq = 1,
-                  reg_lambda = 0,
-                  reg_alpha = 0,
-                  min_data_in_leaf = 1,
-                  colsample_bynode = 1/3)
 
-dtrain_lgb <- lgb.Dataset(X, label = y)
+system.time({
+  param_lgb <- list(boosting = "rf",
+                    max_depth = 10,
+                    num_leaves = 1000,
+                    learning_rate = 1,
+                    objective = "regression",
+                    bagging_fraction = 0.63,
+                    bagging_freq = 1,
+                    reg_lambda = 0,
+                    reg_alpha = 0,
+                    min_data_in_leaf = 5,
+                    colsample_bynode = mtry_p)
+  
+  dtrain_lgb <- lgb.Dataset(X_train, label = y_train)
+  
+  fit_lgb <- lgb.train(param_lgb,
+                       dtrain_lgb, nthread=8,
+                       verbose = -1,
+                       nrounds = 500)
+  
+  pred_lgb <- predict(fit_lgb, X_valid)
+})
 
-fit_lgb <- lgb.train(param_lgb,
-                     dtrain_lgb,
-                     nrounds = 500)
+# Ranger
+system.time({
+  fit_ranger <- ranger(y = y_train, 
+                       x = X_train, 
+                       mtry = mtry,
+                       min.node.size = 5,
+                       max.depth = 10, 
+                       num.trees = 500)
+  
+  pred_ranger <- predict(fit_rf, X_valid)$predictions
+})
 
-# True Random Forest
-fit_rf <- ranger(y = y, 
-                 x = X, 
-                 max.depth = 10, 
-                 num.trees = 500)
+# Rborist
+system.time({
+  fit_rbor <- Rborist(x = X_train, 
+                      y = y_train, 
+                      nTree = 500, 
+                      autoCompress = mtry_p,  
+                      minInfo = 0,
+                      nLevel = 10,
+                      minNode = 5)
+  pred_rbor <- predict(fit_rbor, X_valid)$yPred
+})
+
+system.time({
+  fit_rfsrc <- rfsrc(reformulate(colnames(X_train), "y_train"), 
+                     data = data.frame(X_train, y_train), 
+                     mtry = mtry,
+                     ntree = 500, 
+                     nodedepth = 10,
+                     nodesize = 5,
+                     seed = 837363)
+  pred_rfsrc <- predict(fit_rfsrc, data.frame(X_valid))$predicted
+})
 
 # Evaluate predicitons
 pred <- data.frame(
-  pred_xgb = predict(fit_xgb, X),
-  pred_lgb = predict(fit_lgb, X),
-  pred_rf = predict(fit_rf, X)$predictions
+  pred_xgb,
+  pred_lgb,
+  pred_ranger,
+  pred_rbor,
+  pred_rfsrc
 )
 
 summary(pred) 
 cor(pred)
 
-rmse <- function(y, pred) {
+rmse <- function(pred, y) {
   sqrt(mean((y - pred)^2))
 }
 
-rmse(y, pred$pred_xgb) # 0.6170593
-rmse(y, pred$pred_lgb) # 0.6047725
-rmse(y, pred$pred_rf)  # 0.6003823
+sapply(pred, rmse, y_valid)
 
 sessionInfo()
